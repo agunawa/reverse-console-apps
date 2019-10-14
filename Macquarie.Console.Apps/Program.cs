b85c69fd91da93329b60bce1d5611b4eeb4c1863
agunawa <agunawa@users.noreply.github.com>
2019-10-14 21:52:48 +1100

    initial commit

---
+++ Macquarie.Console.Apps/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Macquarie.Console.Apps
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Please type in your firstname");
+            var userInput = ReadLine();
+            while (string.IsNullOrWhiteSpace(userInput))
+            {
+                WriteLine("Please type in your firstname");
+                userInput = ReadLine();
+            }
+
+
+            char[] array = userInput.ToCharArray();
+            Array.Reverse(array);
+            string reverse = new string(array);
+            string result = $"Hello {userInput}, your name is {userInput.Length} characters in length and backward it is {reverse}";
+            WriteLine(result);
+            ReadKey();
+        }
+    }
+}
